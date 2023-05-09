ca6823106f1e8c4e96272e37dc963a5009bdbecb
Charles Mariot (42) <contact@charles-mariot.fr>
2023-05-09 19:22:52 +0200

    Initial commit

--- module00/ex00/Hello.py
+++ module00/ex00/Hello.py
@@ -1,11 +1,11 @@
-ft_list     = ["Hello", "tata!"]
-ft_tuple    = ("Hello", "toto!")
-ft_set      = {"Hello", "tutu!"}
-ft_dict     = {"Hello" : "titi!"}
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
 
 
 # List items are ordered, changeable, and allow duplicate values.
-# List items are indexed, the first item has index [0], the second item has index [1] etc.
+# List items are indexed, the first item has index [0], the second [1] etc.
 ft_list[1] = "World!"
 
 
@@ -15,7 +15,8 @@ ft_tuple = ("Hello", "France!")
 
 # A set is a collection which is unordered, unchangeable*, and unindexed.
 # Set items are unchangeable, but you can remove items and add new items.
-# Sets are unordered, so you cannot be sure in which order the items will appear.
+# Sets are unordered, so you cannot be sure in which order the items will
+# appear.
 ft_set.remove("tutu!")
 ft_set.add("Paris!")
 
--- module00/ex01/format_ft_time.py
+++ module00/ex01/format_ft_time.py
@@ -1,7 +1,9 @@
 import time
 
+
 def print_seconds():
     second: float = time.time_ns() / 10e9
+    print(second)
 
 
 def print_date():
